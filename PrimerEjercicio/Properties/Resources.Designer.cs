8ed45a9a72b29876fe7d67a6f02c4f7092d1faf7
Veronica <vgalbicka@gmail.com>
2025-04-10 01:55:38 -0300

    add

--- PrimerEjercicio/Properties/Resources.Designer.cs
+++ PrimerEjercicio/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace PrimerEjercicio.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap gradient_2 {
+            get {
+                object obj = ResourceManager.GetObject("gradient_2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
